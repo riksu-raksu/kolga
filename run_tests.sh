9053454e39c65d969c0e6facc539b82b431568ce
riku <5541148+riksu-raksu@users.noreply.github.com>
2020-08-19 12:43:56 +0300

    Diagnostics

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 docker-compose down -v
+whoami
+pwd
+ls -l tests
+ls -l tests/registry
+ls -l tests/registry/certs
 sudo docker-compose --verbose up -V --abort-on-container-exit --exit-code-from client "$@"
