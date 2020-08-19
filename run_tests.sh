f373ecac0deee21811e9a0152bf68190ec928286
riku <5541148+riksu-raksu@users.noreply.github.com>
2020-08-19 12:36:09 +0300

    Maybe more priviliges are needed?

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 docker-compose down -v
-docker-compose --verbose up -V --abort-on-container-exit --exit-code-from client "$@"
+sudo docker-compose --verbose up -V --abort-on-container-exit --exit-code-from client "$@"
