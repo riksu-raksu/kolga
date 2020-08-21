d39db263bb8104aea49ab43ae32a7007ec046044
riku <5541148+riksu-raksu@users.noreply.github.com>
2020-08-21 09:53:01 +0300

    Diagnostics

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+echo SNAPS
+snap list
+echo PACKAGES
+apt list --installed
 docker-compose --verbose down -v
 docker-compose --verbose up -V --abort-on-container-exit --exit-code-from client "$@"
