89e668e0d8e96eff3b95ba5322951548afacca3d
riku <5541148+riksu-raksu@users.noreply.github.com>
2020-08-21 09:28:53 +0300

    Diagnostics

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-docker-compose down -v
-whoami; groups
-sudo usermod -a -G docker vsts_azpcontainer
-bash -lic "whoami; groups"
+docker-compose --verbose down -v
 docker-compose --verbose up -V --abort-on-container-exit --exit-code-from client "$@"
